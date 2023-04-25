66be335ccbfcf735b09ffd6c474dc1d8620bc2b8
navid kianfar <navid.kianfar@outlook.com>
2023-04-26 02:42:13 +0300

    loading component #1

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/auth/auth.page";
 import ChatsPage from "./pages/chats/chats.page";
 import ErrorPage from "./pages/error/error.page";
+import Loading from "./components/loading/loading.component";
 
 const App = () => {
+  let loading = true;
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <BrowserRouter>
       <Routes>
---
+++ src/components/loading/loading.style.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-color: var(--color-background);
+`;
+
+export default Wrapper;
