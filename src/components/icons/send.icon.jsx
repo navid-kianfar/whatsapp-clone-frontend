456246bbb0f3f406ea0383a3755ac0aded5a0350
navid kianfar <navid.kianfar@outlook.com>
2023-04-27 05:08:12 +0300

    before footer styling

--- src/components/icons/send.icon.jsx
+++ src/components/icons/send.icon.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const SendIcon = () => {
-  return <div>SendIcon</div>;
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      height="24"
+      width="24"
+      preserveAspectRatio="xMidYMid meet"
+      version="1.1"
+      x="0px"
+      y="0px"
+      enableBackground="new 0 0 24 24"
+      xmlSpace="preserve"
+    >
+      <path
+        fill="currentColor"
+        d="M1.101,21.757L23.8,12.028L1.101,2.3l0.011,7.912l13.623,1.816L1.112,13.845 L1.101,21.757z"
+      ></path>
+    </svg>
+  );
 };
 
 export default SendIcon;
